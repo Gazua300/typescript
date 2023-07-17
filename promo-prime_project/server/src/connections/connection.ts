d9512713b326a680bd1e90ad66df7e7a9ba95ced
Flamarion França <53473894+Gazua300@users.noreply.github.com>
2023-07-17 22:45:04 +0000

    testantdo mudanças feitas no code online

--- promo-prime_project/server/src/connections/connection.ts
+++ promo-prime_project/server/src/connections/connection.ts
@@ -1,13 +1,16 @@
 import knex from "knex"
+import { config } from 'dotenv'
+
+config()
 
 
 const con = knex({
     client: 'mysql2',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'alfadb',
-        database: 'promo_prime'
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_SCHEMA
     }
 })
 
